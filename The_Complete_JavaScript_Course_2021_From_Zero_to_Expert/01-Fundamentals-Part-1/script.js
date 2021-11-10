5d16614f3ff04f64955a742f48ce67b42ea9c711
Iván E. Hernández T <iamieht@gmail.com>
2021-11-10 15:23:00 +0100

    Update script.js

--- The_Complete_JavaScript_Course_2021_From_Zero_to_Expert/01-Fundamentals-Part-1/script.js
+++ The_Complete_JavaScript_Course_2021_From_Zero_to_Expert/01-Fundamentals-Part-1/script.js
@@ -3,3 +3,13 @@ console.log(40 + 8 + 23 - 10);
 
 let firstName = "Bob";
 console.log(firstName);
+
+// Template Strings
+const myName = "Ivan";
+const myAge = 44;
+
+const ivan = "I'm " + myName + " I'm " + myAge + " years old";
+console.log(ivan);
+
+const ivan2 = `I'm ${myName} and I'm ${myAge} years old`;
+console.log(ivan2);
