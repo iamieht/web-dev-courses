3e05210c11912456dc041b41ce5efe92446ee37a
Iván E. Hernández T <iamieht@gmail.com>
2021-11-22 13:06:01 +0100

    Update script.js

--- The_Complete_JavaScript_Course_2021_From_Zero_to_Expert/01-Fundamentals-Part-1/script.js
+++ The_Complete_JavaScript_Course_2021_From_Zero_to_Expert/01-Fundamentals-Part-1/script.js
@@ -21,3 +21,19 @@ if() {
  } else {
 
 } */
+
+// Type Conversion: Explicity convert from one type to another
+
+const inputYear = "1991";
+console.log(Number(inputYear));
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Jonas"));
+console.log(typeof NaN);
+
+console.log(String(23));
+
+// Type Coercion: JavaScript will dynamically convert the types
+console.log("I am " + 23 + " years old");
+console.log("23" + "10" + 3); // number will be converted to string and concatenated: '23103'
+console.log("23" - "10" - 3); // strings will be converterted to numbers and the result is 10
